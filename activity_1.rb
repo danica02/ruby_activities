1ba24e9c31d84d2c368f18ae37c4fab36ba3f6aa
danica02 <danica.limon1996@gmail.com>
2022-05-07 13:02:44 +0800

    first activity

---
+++ activity_1.rb
@@ -0,0 +1,39 @@
+# Solution #1
+# array = [1,2,3,4,5,6,7,8,9,10]
+# array.each { |x| puts x }
+
+
+# Solution #2
+# h = {"a" => 1, "b" => 2, "c" => 3, "d" => 4}
+# puts h["b"]
+# h["e"] = 5
+# puts h
+
+#Solution #3
+# contact_data = [["john@email.com", "123 Main st.", "555-123-4567"],
+#             ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+# contacts = {"John Cruz" => {}, "Avion School" => {}}
+
+# contacts["John Cruz"] = {"email" => contact_data[0][0], "address" => contact_data[0][1], "phone" => contact_data[0][2]}
+# contacts["Avion School"] = {"email" => contact_data[1][0], "address" => contact_data[1][1], "phone" => contact_data[1][2]}
+
+# puts contacts
+
+
+#Solution #4
+puts "How old are you?"
+puts age = 20
+
+puts "In 10 years you will be :"
+puts age + 10
+
+puts "In 20 years you will be :"
+puts age + 20
+
+puts "In 30 years you will be :"
+puts age + 30
+
+puts "In 40 years you will be :"
+puts age + 40
+
