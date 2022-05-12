f0cafc12370eaf4fff5eb67b29dd8a5bd5c35303
danica02 <danica.limon1996@gmail.com>
2022-05-12 20:50:53 +0800

    Third Activity

--- activity_3.rb
+++ activity_3.rb
@@ -8,29 +8,40 @@ class Dog
     end
 end
 
+my_pet = Dog.new('Ginger', 'Siberian Husky', 5)
+  
+puts my_pet.name
+my_pet.name = 'Parker'
+  
+my_pet.age = 6
+  
+puts my_pet.name
+puts my_pet.age
+puts my_pet.breed
 
-#Activity 2
-class Profile
-    attr_accessor :full_name, :age, :address, :work
-    def initialize(full_name, age, address, work)
-        @full_name = full_name
-        @age = age
-        @address = address
-        @work = work
-    end
-end
+
+# #Activity 2
+# class Profile
+#     attr_accessor :full_name, :age, :address, :work
+#     def initialize(full_name, age, address, work)
+#         @full_name = full_name
+#         @age = age
+#         @address = address
+#         @work = work
+#     end
+# end
   
-my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
+# my_profile = Profile.new('Juan', 18, 'Bulacan', 'Instructor')
   
-puts my_profile.full_name
-my_profile.full_name = 'Juan Cruz'
+# puts my_profile.full_name
+# my_profile.full_name = 'Juan Cruz'
   
-my_profile.age = 25
-my_profile.work = 'Software Engineer'
+# my_profile.age = 25
+# my_profile.work = 'Software Engineer'
   
-puts my_profile.full_name
-puts my_profile.age
-puts my_profile.work
-puts my_profile.address
+# puts my_profile.full_name
+# puts my_profile.age
+# puts my_profile.work
+# puts my_profile.address
   
   
