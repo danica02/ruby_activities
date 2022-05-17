86647fb70974c536ddf41cf474badfb1b0573c7b
danica02 <danica.limon1996@gmail.com>
2022-05-17 21:03:48 +0800

    Six Activity

---
+++ activity_6.rb
@@ -0,0 +1,18 @@
+arr1 = [34, 15, 88, 2]
+arr2 = [43, -345, -1, 100]
+
+def smallest_int(arr)
+    a = nil
+    arr.each do |x|
+        if a == nil
+            a = x
+        elsif x < a
+            a = x
+        end
+    end
+    puts a
+end
+
+smallest_int (arr1)
+smallest_int (arr2)
+
