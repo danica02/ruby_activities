8b2a0dde6102737f14451c43b44f7a03f59251e1
danica02 <danica.limon1996@gmail.com>
2022-05-19 19:47:57 +0800

    Database Second Activity

---
+++ database/db_activity2.sql
@@ -0,0 +1,21 @@
+SELECT * FROM students;
+
+-----
+
+SELECT COUNT(*)
+FROM students;
+
+-----
+
+SELECT * FROM students
+WHERE location = 'Manila';
+
+-----
+
+SELECT AVG(age)
+FROM students;
+
+-----
+
+SELECT * FROM students
+ORDER BY age DESC;
