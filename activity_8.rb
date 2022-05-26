d8d72edec7fd2bf735ebeded2c4ef68f2d649fde
danica02 <danica.limon1996@gmail.com>
2022-05-26 22:10:37 +0800

    Eight Activity

--- activity_8.rb
+++ activity_8.rb
@@ -1,17 +1,16 @@
 def unique_in_order(iterable)
 newArray = []
 
-    if iterable.is_a?(Array)
-        newArray.push iterable
+    if iterable.is_a?(String)
+        iterable.split("").each {|char| puts char}
     else
-        iterable.each_char do |x|
-            return false if newArray.include? x
-            newArray.push x
-        end
+        newArray.push iterable
     end
-    puts newArray.length
+    puts newArray
 end
 
 unique_in_order('AAAABBBCCDAABBB')
 unique_in_order('ABBCcAD')
 unique_in_order([1,2,2,3,3])
+
+
